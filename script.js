473580e64a3fe69f3a8191faf6bb51113519c28a
Fedorova0410 <50097427+Fedorova0410@users.noreply.github.com>
2020-03-18 10:39:20 +0200

    day 1 done, 18.03.2020

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 const formSearch = document.querySelector('.form-search'),
-    inputCitiesForm = document.querySelector('.input__cities-from '),
-    dropdownCitiesForm = document.querySelector('.dropdown__cities-from'),
+    inputCitiesFrom = document.querySelector('.input__cities-from '),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
     inputCitisTo = document.querySelector('.input__cities-to')
     ;
 
-const city = ['Харьков', 'Санкт-Петербург', 'Минск', 'Карaганда', 'Челябинск', 'Керч', 'Волгоград', 'Самара', 'Днепропетровск', 'Ектеринбург', 'Одесса', 'Ухань', 'Шишкен', 'Нижний новгород', 'Калининград', 'Ворцлав', 'Ростов-на-дону', 'Москва'];
+const city = ['Харьков', 'Санкт-Петербург', 'Минск', 'Карaганда', 'Челябинск', 'Керчь', 'Волгоград', 'Самара', 'Днепропетровск', 'Ектеринбург', 'Одесса', 'Ухань', 'Шишкен', 'Нижний новгород', 'Калининград', 'Ворцлав', 'Ростов-на-Дону', 'Москва'];
 
-inputCitiesForm.addEventListener('input', () => {
-    dropdownCitiesForm.textContent = '';
+const showCity = (input, list) => {
+    list.textContent = '';
 
-    if (inputCitiesForm.value !== '') {
-
-
+    if (input.value !== '') {
 
         const filterCity = city.filter((item) => {
             const fixItem = item.toLowerCase();
 
-            return fixItem.includes(inputCitiesForm.value.toLowerCase())
+            return fixItem.includes(input.value.toLowerCase())
         });
 
         filterCity.forEach((item) => {
             const li = document.createElement('li');
             li.classList.add('dropdown__city');
             li.textContent = item;
-            dropdownCitiesForm.append(li)
+            list.append(li)
         });
     }
+}; 
 
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom)
 });
 
-const get = (name) => {
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    const target = event.target;
 
-}
+    if (target.tagName.toLowerCase() === 'li') {
+        inputCitiesFrom.value = target.textContent;
+        dropdownCitiesFrom.textContent = '';
+    }
+})
+
